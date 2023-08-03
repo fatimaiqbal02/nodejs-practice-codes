8b6a80419914bd6d16e32597cb33b046d5d96f60
FatimaIqbal <fatimaiqbalmirza002@gmail.com>
2023-08-03 12:41:21 +0500

    Emitting and Handling CustomEvents

---
+++ 15-Emitting&HandlingCustomEvents.js
@@ -0,0 +1,17 @@
+const events = require('events')
+
+//created event emitter
+myEmitter = new events.EventEmitter()
+
+//event listener and event handler 
+myEmitter.on('userCreated', (name, id)=>{
+    console.log(`A user is created with name = ${name} and ID = ${id}`)
+})
+
+//we can make another event listener and event handler for same event
+myEmitter.on('userCreated', (name, id)=>{
+    console.log(`A user is already created with name = ${name} and ID = ${id}`)
+})
+
+//event emitter emits an event
+myEmitter.emit('userCreated', 'fatima', 17)
