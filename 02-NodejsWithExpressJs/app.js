80b5b8fbe3658b7867dcc61d660350ddac90d420
FatimaIqbal <fatimaiqbalmirza002@gmail.com>
2023-08-08 21:39:39 +0500

    create API and handle get request

--- 02-NodejsWithExpressJs/app.js
+++ 02-NodejsWithExpressJs/app.js
@@ -1,11 +1,20 @@
 const express = require('express')
-let app = express()
+let fs = require('fs')
 
+let app = express()
 const portNo = 3000
 
-//route => http method + url
-app.get('/', (req, res)=>{
-    res.status(200).json({message: 'welcome', status: 200})
+let movies = JSON.parse(fs.readFileSync('./data/movies.json'))
+
+//GET: api/v1/movies
+app.get('/api/v1/movies', (req,res)=>{
+    res.status(200).json({
+        status: "success",
+        count: movies.length,
+        data: {
+            movies: movies
+        }
+    })
 })
 
 //creating and starting the server
